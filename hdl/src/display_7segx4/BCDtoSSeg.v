aa09819dd76bb12e7e3cc19d42f6915e0091a48b
Luis Garcia <luisf.garciar@ecci.edu.co>
2020-04-01 17:55:03 -0500

    Update BCDtoSSeg.v

--- hdl/src/display_7segx4/BCDtoSSeg.v
+++ hdl/src/display_7segx4/BCDtoSSeg.v
@@ -1,28 +1,35 @@
-module BCDtoSSeg (BCD, SSeg);
+module BCDtoSSeg (EQDEC, SSeg);
 
-  input [3:0] BCD;
+  input [3:0]EQDEC;
   output reg [6:0] SSeg;
-always @ ( * ) begin
-  case (BCD)
-   4'b0000: SSeg = 7'b0000001; // "0"  
-	4'b0001: SSeg = 7'b1001111; // "1" 
-	4'b0010: SSeg = 7'b0010010; // "2" 
-	4'b0011: SSeg = 7'b0000110; // "3" 
-	4'b0100: SSeg = 7'b1001100; // "4" 
-	4'b0101: SSeg = 7'b0100100; // "5" 
-	4'b0110: SSeg = 7'b0100000; // "6" 
-	4'b0111: SSeg = 7'b0001111; // "7" 
-	4'b1000: SSeg = 7'b0000000; // "8"  
-	4'b1001: SSeg = 7'b0000100; // "9" 
+
+  always @ ( * ) begin
+  
+  case (EQDEC)
+   0: SSeg = 7'b0000001; // "0"  
+	1: SSeg = 7'b1001111; // "1" 
+	2: SSeg = 7'b0010010; // "2" 
+	3: SSeg = 7'b0000110; // "3" 
+	4: SSeg = 7'b1001100; // "4" 
+	5: SSeg = 7'b0100100; // "5" 
+	6: SSeg = 7'b0100000; // "6" 
+	7: SSeg = 7'b0001111; // "7" 
+	8: SSeg = 7'b0000000; // "8"  
+	9: SSeg = 7'b0000100; // "9" 
+ 
+ /*
    4'ha: SSeg = 7'b0001000;  
    4'hb: SSeg = 7'b1100000;
    4'hc: SSeg = 7'b0110001;
-   4'hd: SSeg = 7'b1000010;
+   4'hd: SSeg = 7'b1000010;  
    4'he: SSeg = 7'b0110000;
    4'hf: SSeg = 7'b0111000;
-    default:
-    SSeg = 0;
-  endcase
-end
+  */
+
+  // default:
+  // SSeg = 7'b1111111;
+	endcase
+
+	end
 
 endmodule
