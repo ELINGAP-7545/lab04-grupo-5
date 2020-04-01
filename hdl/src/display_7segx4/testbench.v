22a12e301a40bd3c93f4b3f761d8acc9f9c0ec04
Luis Garcia <luisf.garciar@ecci.edu.co>
2020-04-01 17:55:34 -0500

    Update testbench.v

--- hdl/src/display_7segx4/testbench.v
+++ hdl/src/display_7segx4/testbench.v
@@ -1,41 +1,23 @@
 `timescale 1ns / 1ps
 
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   22:17:21 09/05/2019
-// Design Name:   display
-// Module Name:   C:/Users/UECCI/Desktop/ejer01/display_7segx4/testbench.v
-// Project Name:  display_7segx4
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: display
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
 
 module testbench;
 
 	// Inputs
-	reg [15:0] num;
+
+	reg [15:0] numero;
 	reg clk2;
 	reg rst;
 
 	// Outputs
 	wire [0:6] sseg;
-	wire [3:0] an;
+	wire [4:0] an;
+	
 
 	// Instantiate the Unit Under Test (UUT)
 	display uut (
-	//	.num(num), 
+	
+		.num1(numero), 
 		.clk(clk2), 
 		.sseg(sseg), 
 		.an(an), 
@@ -43,18 +25,18 @@ module testbench;
 	);
 
 	initial begin
+	
 		// Initialize Inputs
+		
 		clk2= 0;
 		rst = 1;
-		#10 rst =0;
-		
-		num = 16'h4321;
-        
+#20;	rst =0;
+	
+	numero = 20007;   
 
-	end
-      
+   	end   
 
 	always #1 clk2 = ~clk2;
-	
+
 endmodule
 
